f4fb9a2865b18a1180d6742d8ae5e0668f0f4729
zisam-glitch <128255158+zisam-glitch@users.noreply.github.com>
2023-04-18 14:12:15 +0600

    llm

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -3,6 +3,8 @@ import "./Blog.scss";
 import { fetchDataFromApi } from "../../utils/api";
 import { Context } from "../../utils/context";
 import Post from "./Post/Post";
+import { Helmet } from 'react-helmet';
+
 
 const Blog = () => {
     const { products, setProducts, categories, setCategories, posts, setPosts } =
@@ -32,9 +34,25 @@ const Blog = () => {
 
     return (
         <>
-        <div className="title-blog">
-            <h1>Our Latest News</h1>
-        </div>
+            <div className="title-blog">
+                <Helmet>
+                    <meta charset="UTF-8" />
+                    <meta name="viewport" content="width=device-width, initial-scale=1" />
+                    <link rel="profile" href="https://gmpg.org/xfn/11" />
+                    <title>Blog - Air Purification System Against Viruses</title>
+                    <meta name="description" content="Check This Blog on Can an Air Purifier Eliminates Viruses And How To Reduce Fallow Time in Dentistry." />
+                    <link rel="canonical" href="https://marspurifier.com/blog/" />
+                    <link rel="next" href="https://marspurifier.com/blog/page/2/" />
+                    <meta property="og:locale" content="en_GB" />
+                    <meta property="og:type" content="article" />
+                    <meta property="og:title" content="Blog - Air Purification System Against Viruses" />
+                    <meta property="og:description" content="Check This Blog on Can an Air Purifier Eliminates Viruses And How To Reduce Fallow Time in Dentistry." />
+                    <meta property="og:url" content="https://marspurifier.com/blog/" />
+                    <meta property="og:site_name" content="Dental Air Purification - Accredited" />
+                    <meta name="twitter:card" content="summary_large_image" />
+                </Helmet>
+                <h1>Our Latest News</h1>
+            </div>
             <div>
                 <Post posts={posts} />
             </div>
--- src/components/SinglePost/singlePost.jsx
+++ src/components/SinglePost/singlePost.jsx
@@ -2,36 +2,42 @@ import { useParams } from "react-router-dom";
 import useFetch from "../../hooks/useFetch";
 import ReactMarkdown from 'react-markdown'
 import "./SinglePost.scss";
+import { Helmet } from 'react-helmet';
 
 const SinglePost = () => {
 
     const { id } = useParams();
     const { data } = useFetch(`/api/posts?populate=*&[filters][id]=${id}`);
 
-  
 
 
-    if(!data) return;
+
+    if (!data) return;
     const post = data?.data[0].attributes
 
     return (
         <>
-        <div className="sg-post">
-            <div className="titl-sg">{post.title}</div>
-            <div className="img">
-               <img src={process.env.REACT_APP_DEV_URL +
-               post.img.data.attributes.url } alt="" />
-            </div >
-            <div className="infx">
-            <span className="author">By: {post.author}</span>
-            <span> - </span>
-            <span className="date">On: {post.date}</span>
+            <div className="sg-post">
+                <Helmet>
+                    <meta charset="UTF-8" />
+                    <meta name="viewport" content="width=device-width, initial-scale=1" />
+                    <title>{post.title}</title>
+                </Helmet>
+                <div className="titl-sg">{post.title}</div>
+                <div className="img">
+                    <img src={process.env.REACT_APP_DEV_URL +
+                        post.img.data.attributes.url} alt="" />
+                </div >
+                <div className="infx">
+                    <span className="author">By: {post.author}</span>
+                    <span> - </span>
+                    <span className="date">On: {post.date}</span>
+                </div>
+                <ReactMarkdown>
+                    {post.desc}
+                </ReactMarkdown>
+                <div className="trd">{post.descx}</div>
             </div>
-            <ReactMarkdown>
-            {post.desc}
-            </ReactMarkdown>
-            <div className="trd">{post.descx}</div>
-        </div>
         </>
     );
 };
