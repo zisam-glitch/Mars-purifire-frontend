a0099d60562a14cc14c32e65d4249064977db269
zisam-glitch <128255158+zisam-glitch@users.noreply.github.com>
2023-05-08 18:47:00 +0600

    kasy

--- src/components/SingleProduct/SingleProduct.jsx
+++ src/components/SingleProduct/SingleProduct.jsx
@@ -116,23 +116,188 @@ const SingleProduct = () => {
                             <div
                                 className={toggleState === 1 ? "content  active-content" : "content"}
                             >
-                                    {product.m}
+                                <ReactMarkdown>
+                                    {product.mainDesc}
+                                </ReactMarkdown>
                             </div>
 
                             <div
                                 className={toggleState === 2 ? "content  active-content" : "content"}
                             >
-                                <ReactMarkdown>
-                                    {product.specification}
-                                </ReactMarkdown>
+                                <h1>{product.jsom[0]?.title}</h1>
+                                <table className="table-mn">
+                                    <tr>
+                                        <th className="table-mntr">{product.jsom[0]?.title}</th>
+
+                                    </tr>
+                                    <tr>
+                                        <td>{product.jsom[0]?.text}</td>
+                                        <td>{product.jsom[0]?.value}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>{product.jsom[1]?.text}</td>
+                                        <td>{product.jsom[1]?.value}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>{product.jsom[2]?.text}</td>
+                                        <td>{product.jsom[2]?.value}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>{product.jsom[3]?.text}</td>
+                                        <td>{product.jsom[3]?.value}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>{product.jsom[4]?.text}</td>
+                                        <td>{product.jsom[4]?.value}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>{product.jsom[5]?.text}</td>
+                                        <td>{product.jsom[5]?.value}</td>
+                                    </tr>
+                                </table>
+                                <h1>{product.jsom1[0]?.title}</h1>
+                                <table className="table-mn">
+                                    <tr>
+                                        <th className="table-mntr">{product.jsom1[1]?.title}</th>
+
+                                    </tr>
+                                    <tr>
+                                        <td>{product.jsom1[0]?.text}</td>
+                                        <td>{product.jsom1[0]?.value}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>{product.jsom1[1]?.text}</td>
+                                        <td>{product.jsom1[1]?.value}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>{product.jsom1[2]?.text}</td>
+                                        <td>{product.jsom1[2]?.value}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>{product.jsom1[3]?.text}</td>
+                                        <td>{product.jsom1[3]?.value}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>{product.jsom1[4]?.text}</td>
+                                        <td>{product.jsom1[4]?.value}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>{product.jsom1[5]?.text}</td>
+                                        <td>{product.jsom1[5]?.value}</td>
+                                    </tr>
+
+                                </table>
+                                <h1>{product.jsom2[0]?.title}</h1>
+                                <table className="table-mn">
+                                    <tr>
+                                        <th className="table-mntr">{product.jsom2[1]?.title}</th>
+
+                                    </tr>
+                                    <tr>
+                                        <td>{product.jsom2[0]?.text}</td>
+                                        <td>{product.jsom2[0]?.value}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>{product.jsom2[1]?.text}</td>
+                                        <td>{product.jsom2[1]?.value}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>{product.jsom2[2]?.text}</td>
+                                        <td>{product.jsom2[2]?.value}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>{product.jsom2[3]?.text}</td>
+                                        <td>{product.jsom2[3]?.value}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>{product.jsom2[4]?.text}</td>
+                                        <td>{product.jsom2[4]?.value}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>{product.jsom2[5]?.text}</td>
+                                        <td>{product.jsom2[5]?.value}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>{product.jsom2[6]?.text}</td>
+                                        <td>{product.jsom2[6]?.value}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>{product.jsom2[7]?.text}</td>
+                                        <td>{product.jsom2[7]?.value}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>{product.jsom2[8]?.text}</td>
+                                        <td>{product.jsom2[8]?.value}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>{product.jsom2[9]?.text}</td>
+                                        <td>{product.jsom2[9]?.value}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>{product.jsom2[10]?.text}</td>
+                                        <td>{product.jsom2[10]?.value}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>{product.jsom2[11]?.text}</td>
+                                        <td>{product.jsom2[11]?.value}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>{product.jsom2[12]?.text}</td>
+                                        <td>{product.jsom2[12]?.value}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>{product.jsom2[13]?.text}</td>
+                                        <td>{product.jsom2[13]?.value}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>{product.jsom2[14]?.text}</td>
+                                        <td>{product.jsom2[14]?.value}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>{product.jsom2[15]?.text}</td>
+                                        <td>{product.jsom2[15]?.value}</td>
+                                    </tr>
+                                </table>
+                                <h1>{product.jsom3[0]?.title}</h1>
+                                <table className="table-mn">
+                                    <tr>
+                                        <th className="table-mntr">{product.jsom3[1]?.title}</th>
+                                        <th className="table-mntr">{product.jsom3[2]?.title}</th>
+                                    </tr>
+                                    <tr>
+                                        <td>{product.jsom3[0]?.text}</td>
+                                        <td>{product.jsom3[0]?.value}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>{product.jsom3[1]?.text}</td>
+                                        <td>{product.jsom3[1]?.value}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>{product.jsom3[2]?.text}</td>
+                                        <td>{product.jsom3[2]?.value}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>{product.jsom3[3]?.text}</td>
+                                        <td>{product.jsom3[3]?.value}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>{product.jsom3[4]?.text}</td>
+                                        <td>{product.jsom3[4]?.value}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>{product.jsom3[5]?.text}</td>
+                                        <td>{product.jsom3[5]?.value}</td>
+                                    </tr>
+
+                                </table>
                             </div>
 
                             <div
                                 className={toggleState === 3 ? "content  active-content" : "content"}
                             >
-                                
-                                    {product.jsom}
-                               
+                                <ReactMarkdown>
+                                    {product.support}
+                                </ReactMarkdown>
                             </div>
                         </div>
                     </div>
@@ -142,6 +307,7 @@ const SingleProduct = () => {
                     categoryId={product.categories.data[0].id}
                 />
             </div>
+
         </div>
     );
 };
